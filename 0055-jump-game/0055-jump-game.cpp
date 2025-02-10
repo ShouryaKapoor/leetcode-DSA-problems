d7f295cb902c58027ece9b8d5e6353cd66df09b4
Shourya Kapoor <112488155+ShouryaKapoor@users.noreply.github.com>
2025-02-10 20:20:14 +0530

    Time: 0 ms (100%), Space: 52.3 MB (61.08%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        //storing the size of nums
         int n = nums.size();
+        //taking answer  to be 0 as this compares the remaining jumps in the array 
         int ans = 0;
-        for(int i=0;i<n;i++)
+        for(int i = 0;i<n;i++) //iterating the array
         {
-            if(i>ans)
+            if(i>ans) //if index values go over the answer range then it is a false case...insufficient steps
             {
                 return false;
             }
-        ans = max(ans,nums[i]+i);
+            ans = max(ans, nums[i]+i); //updating ans value till a greater element comes after the ith element
         }
-        return true;
-        
+        return true; //return true when we reach the end of array
     }
 };
