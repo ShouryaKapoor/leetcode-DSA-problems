fb52cb52dfa4746946d0c51e2c52e08858c93b77
Shourya Kapoor <112488155+ShouryaKapoor@users.noreply.github.com>
2025-02-20 13:48:46 +0530

    Time: 0 ms (100%), Space: 12.5 MB (84%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+        string result = "";
+        for (int i = 0; i < n; i++) {
+            // Flip the diagonal elements
+            result += (nums[i][i] == '0') ? '1' : '0';
+        }
+        return result;
+    }
+};
