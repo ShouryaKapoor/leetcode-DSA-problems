da5ed86bc504a846a2084ddc30c1e56ac02415d3
Shourya Kapoor <112488155+ShouryaKapoor@users.noreply.github.com>
2025-02-21 11:05:42 +0530

    Time: 375 ms (42.17%), Space: 185.6 MB (32.35%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
+
+     void merge(vector<int>& nums, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        vector<int> leftArr(n1), rightArr(n2);
+
+        for (int i = 0; i < n1; i++) leftArr[i] = nums[left + i];
+        for (int j = 0; j < n2; j++) rightArr[j] = nums[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) nums[k++] = leftArr[i++];
+            else nums[k++] = rightArr[j++];
+        }
+
+        while (i < n1) nums[k++] = leftArr[i++];
+        while (j < n2) nums[k++] = rightArr[j++];
+    }
+
+    void mergeSort(vector<int>& nums, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(nums, left, mid);
+            mergeSort(nums, mid + 1, right);
+            merge(nums, left, mid, right);
+        }
+    }
+
     vector<int> sortArray(vector<int>& nums) {
-        quickSort(nums, 0, nums.size() - 1);
+        mergeSort(nums, 0, nums.size() - 1);
         return nums;
     }
-        void quickSort(vector<int>& nums, int low, int high){
-        if(low < high){
-            int pivotIndex = partition(nums, low ,  high);
-            quickSort(nums, low, pivotIndex - 1);
-            quickSort(nums, pivotIndex + 1, high);
-        }
-    }
-    int partition(vector<int>& nums, int low, int high){
-        int pivotElement = nums[high];
-        int i = low - 1;
-        for(int j  = low ; j <= high - 1; j++){
-            if(nums[j] <= pivotElement){
-                i = i+1;
-                swap(nums[j], nums[i]);
-            }
-        }
-        swap(nums[i+1], nums[high]);
-        return i+1;
-    }
-};
+};  
